01f4b99c44561adf4c20668264a9396bcc3d6dcf
aricycle7 <148219989+aricycle7@users.noreply.github.com>
2024-12-19 14:04:46 -0500

    css foxed fpr odfjdosj jfoid

--- javascript/functions.js
+++ javascript/functions.js
@@ -11,50 +11,55 @@ xhr2.onload = function () {
     let responseObject = JSON.parse(xhr2.responseText);
 
     console.log(responseObject);
-    
+
     //create loop and stuff
     for (let i = 0; i < responseObject.Headers.length; i++) {
       let newCard = "";
-      
 
-
-      newCard += "<div class=\"footer_Container\">";
-      newCard += "<div class=\"footer_link_wrapper\">";
-      newCard += "<div class=\"footer_Link_Item\">";
+      newCard += '<div class="footer_Container">';
+      newCard += '<div class="footer_link_wrapper">';
+      newCard += '<div class="footer_Link_Item">';
       newCard += "<h2>" + responseObject.Headers[i].Header1 + "</h2>" + " ";
       // newCard += "<div>"
-      newCard += responseObject.Header1[i].Section1;
-      newCard += responseObject.Header1[i].Section2;
-      newCard += responseObject.Header1[i].Section3;
-      newCard += responseObject.Header1[i].Section4;
+      newCard += "<ul>";
+      newCard += "<li>" + responseObject.Header1[i].Section1 + "</li>";
+      newCard += "<li>" + responseObject.Header1[i].Section2 + "</li>";
+      newCard += "<li>" + responseObject.Header1[i].Section3 + "</li>";
+      newCard += "<li>" + responseObject.Header1[i].Section4 + "</li>";
+      newCard += "</ul>";
       newCard += "</div>";
-      
-      newCard += "<div class=\"footer_Link_Item\">";
+
+      newCard += '<div class="footer_Link_Item">';
       newCard += "<h2>" + responseObject.Headers[i].Header2 + "</h2>" + " ";
-      
-      newCard += responseObject.Header2[i].Section1 + " ";
-      newCard += responseObject.Header2[i].Section2 + " ";
-      newCard += responseObject.Header2[i].Section3 + " ";
+      newCard += "<ul>";
+      newCard += "<li>" + responseObject.Header2[i].Section1 + "</li>";
+      newCard += "<li>" + responseObject.Header2[i].Section2 + "</li>";
+      newCard += "<li>" + responseObject.Header2[i].Section3 + "</li>";
+      newCard += "</ul>";
       newCard += "</div>";
-      
-      newCard += "<div class=\"footer_Link_Item\">";
+
+      newCard += '<div class="footer_Link_Item">';
       newCard += "<h2>" + responseObject.Headers[i].Header3 + "</h2>" + " ";
-      
-      newCard += responseObject.Header3[i].Section1 + " ";
-      newCard += responseObject.Header3[i].Section2 + " ";
+      newCard += "<ul>";
+      newCard += "<li>" + responseObject.Header3[i].Section1 + " ";
+      newCard += "<li>" + responseObject.Header3[i].Section2 + " ";
+      newCard += "</ul>";
       newCard += "</div>";
-      
-      newCard += "<div class=\"social_media\">";
-      newCard += "<div class=\"social_media_wrapper\">";
-      newCard += "<div id=\"footer_logo\">";
-      newCard += "<p class=\"website_rights\" style=\"color:var(--clr-DarkGrey);\">" + "©   STAGE FRIGHT 2024. All rights reserved." + "</p>"; 
-      newCard += "<div id=\"social_media_icons\">";
-      newCard += responseObject.Header4[i].Section1Link + " ";
-      newCard += responseObject.Header4[i].Section2Link + " ";
-      newCard += responseObject.Header4[i].Section3Link + " ";
-      newCard += responseObject.Header4[i].Section4Link + " ";
-      newCard += responseObject.Header4[i].Section5Link + " ";
-      newCard += responseObject.Header4[i].Section6Link + " ";
+
+      newCard += '<div class="social_media">';
+      newCard += '<div class="social_media_wrapper">';
+      newCard += '<div id="footer_logo">';
+      newCard +=
+        '<p class="website_rights" style="color:var(--clr-DarkGrey);">' +
+        "©   STAGE FRIGHT 2024. All rights reserved." +
+        "</p>";
+      newCard += '<div id="social_media_icons">';
+      newCard += responseObject.Header4[i].Section1Link;
+      newCard += responseObject.Header4[i].Section2Link;
+      newCard += responseObject.Header4[i].Section3Link;
+      newCard += responseObject.Header4[i].Section4Link;
+      newCard += responseObject.Header4[i].Section5Link;
+      newCard += responseObject.Header4[i].Section6Link;
       newCard += "</div>";
       newCard += "</div>";
       newCard += "</div>";
@@ -63,11 +68,9 @@ xhr2.onload = function () {
       newCard += "</div>";
 
       document.getElementById("banana").innerHTML += newCard;
-    };
+    }
   }
 };
 
 xhr2.open("GET", "../JSON/footerJSON.json", true);
 xhr2.send(null);
-
-
